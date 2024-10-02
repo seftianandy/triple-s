bfc816ef9a371f1b66af3ce5671bb72596229892
seftianandy <seftianandy45@gmail.com>
2024-10-02 16:45:58 +0700

    Pemasangan admin template
    - halaman login
    - halaman dashboard

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\DashboardController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +16,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [LoginController::class, 'index']);
+Route::get('/dashboard', [DashboardController::class, 'index']);
